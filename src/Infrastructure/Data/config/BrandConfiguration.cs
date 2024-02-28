9b01d8441d067fd5142e71434dd6d332f306661c
Batuhanemregk <batuhan_e_g@hotmail.com>
2024-02-28 12:32:15 +0300

    entity, context

---
+++ src/Infrastructure/Data/config/BrandConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.config
+{
+    public class BrandConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(100);
+        }
+    }
+}
---
+++ src/Infrastructure/Data/config/CategoryConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.config
+{
+    public class CategoryConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(50);
+        }
+    }
+}
